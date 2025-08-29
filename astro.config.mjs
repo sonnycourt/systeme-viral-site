9c048c3d977bd5f25eeccf29c539b1379855df21
sonnycourt <sonnycourt@gmail.com>
2025-08-29 16:25:30 +0200

    feat: intégration complète du design et ajout des effets sonores pour le popup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import tailwindcss from '@tailwindcss/vite';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://systemeviral.com',
@@ -15,9 +13,5 @@ export default defineConfig({
   server: {
       port: 4321,
       host: true,
-  },
-
-  vite: {
-    plugins: [tailwindcss()],
   },
 });
